25ee2ed7b5bc7b10294864411ae4522593e6b15c
Muhammad Moazam <109450741+MuhammadMoazam@users.noreply.github.com>
2023-11-22 21:47:12 +0500

    About page done with CSS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Contact from './components/Contact/Contact';
 import Blog from './components/Blog/Blog';
+import About from './components/About/About'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App()
 {
@@ -14,6 +15,7 @@ function App()
         <Route path='/*' element={<Home />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
         <Route path='/blog' element={<Blog />}></Route>
+        <Route path='/about' element={<About />}></Route>
       </Routes>
     </BrowserRouter>
     
---
+++ src/components/About/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import { Link } from 'react-router-dom'
+const About = () =>
+{
+    return (
+        <>
+            <div className="about">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="titlepage text_align_center">
+                                <h2>About Our Company</h2>
+                            </div>
+                        </div>
+                        <div className="col-md-10 offset-md-1">
+                            <div className="about_text text_align_center">
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentiallyLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+                                </p>
+                                <Link className="read_more" to="/about.html">Read More</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default About
